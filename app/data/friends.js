05bbcde274f5dc6e3575d443e68d97475819e7fb
Rye-Guy <ryanracioppo95@gmail.com>
2018-07-09 20:53:28 -0400

    placeholder data added and form created

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,69 @@
-var friends = [
-    {
+var friends = [{
         name: 'Psycho Pete',
         imgURL: 'https://source.unsplash.com/xe68QiMaDrQ/1600x900',
         score: [
+            2,
+            3,
             1,
+            3,
+            5,
+            4,
+            3,
+            5,
             1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1
-        ] 
+            4
+        ]
 
-    }
-]; 
+    },
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Jacob Deming",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+
+];
 
 module.exports = friends;
